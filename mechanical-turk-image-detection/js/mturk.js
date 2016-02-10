0ce14224ca48bf653cd4d960971cbbac15008df4
Irene Alvarado <ire.alvarado@gmail.com>
2016-02-09 20:47:28 -0500

    Added instructions

--- mechanical-turk-image-detection/js/mturk.js
+++ mechanical-turk-image-detection/js/mturk.js
@@ -11,7 +11,6 @@
 
 // selector used by jquery to identify your form
 var form_selector = "#mturk_form";
-console.log("found the turk form") ;
 
 // function for getting URL parameters
 function gup(name) {
@@ -37,16 +36,11 @@ $(document).ready(function () {
     // Add a new hidden input element with name="assignmentId" that
     // with assignmentId as its value.
     var hitId = gup("hitId") ;
-    console.log("the hit ID") ;
-    console.log(hitId) ;
     var workerId = gup("workerId") ;
-    console.log("the worker ID") ;
-    console.log(workerId) ;
     var aid_input = $("<input type='hidden' name='assignmentId' value='" + aid + "'>").appendTo($(form_selector));
     var aid_input = $("<input type='hidden' name='hitId' value='" + hitId + "'>").appendTo($(form_selector));
     var aid_input = $("<input type='hidden' name='workerId' value='" + workerId + "'>").appendTo($(form_selector));
 
-    console.log("inside function ready setting turk submission options") ;
     // Make sure the submit form's method is POST
     $(form_selector).attr('method', 'POST');
 
--- mechanical-turk-image-detection/js/scripts.js
+++ mechanical-turk-image-detection/js/scripts.js
@@ -5,9 +5,9 @@ $("#draw3").addClass('active');
 $("#draw4").addClass('active');
 $("#draw5").addClass('active');
 
-$('body').on('click', '.btn-group button', function (e) {
+$('body').on('click', '.btn-group button', function(e) {
     $(this).addClass('active');
-    $(this).siblings().removeClass('active');    
+    $(this).siblings().removeClass('active');
 });
 
 var canvasArray = []
@@ -17,99 +17,97 @@ var canvas0 = document.getElementById("canvas0");
 var context0 = canvas0.getContext("2d");
 var canvas1 = document.getElementById("canvas1");
 var context1 = canvas1.getContext("2d");
-var canvas2 = document.getElementById("canvas0");
+var canvas2 = document.getElementById("canvas2");
 var context2 = canvas2.getContext("2d");
-var canvas3 = document.getElementById("canvas1");
+var canvas3 = document.getElementById("canvas3");
 var context3 = canvas3.getContext("2d");
-var canvas4 = document.getElementById("canvas0");
+var canvas4 = document.getElementById("canvas4");
 var context4 = canvas4.getContext("2d");
-var canvas5 = document.getElementById("canvas1");
+var canvas5 = document.getElementById("canvas5");
 var context5 = canvas5.getContext("2d");
 
-canvasArray.push(canvas0) ;
-canvasArray.push(canvas1) ;
-canvasArray.push(canvas2) ;
-canvasArray.push(canvas3) ;
-canvasArray.push(canvas4) ;
-canvasArray.push(canvas5) ;
-contextArray.push(context0) ;
-contextArray.push(context1) ;
-contextArray.push(context2) ;
-contextArray.push(context3) ;
-contextArray.push(context4) ;
-contextArray.push(context5) ;
+canvasArray.push(canvas0);
+canvasArray.push(canvas1);
+canvasArray.push(canvas2);
+canvasArray.push(canvas3);
+canvasArray.push(canvas4);
+canvasArray.push(canvas5);
+contextArray.push(context0);
+contextArray.push(context1);
+contextArray.push(context2);
+contextArray.push(context3);
+contextArray.push(context4);
+contextArray.push(context5);
 //context.globalCompositeOperation = "source-over";
 //context.globalCompositeOperation = "destination-out";
 //context.globalCompositeOperation = "destination-over";
 
-var count = [] ;
-for(var k = 0 ; k < 6 ; k++) {
-    count.push(0) ;
+var count = [];
+for (var k = 0; k < 6; k++) {
+    count.push(0);
 }
 
-var boxes = []; 
-for(var k = 0 ; k < 6 ; k++) {
-    boxes.push([]) ;
+var boxes = [];
+for (var k = 0; k < 6; k++) {
+    boxes.push([]);
 }
 
-var j = 0 ;
-var width = 10 ;
-var height = 10 ;
+var j = 0;
+var width = 8;
+var height = 8;
 
 function Box() {
-  this.x = 0;
-  this.y = 0;
+    this.x = 0;
+    this.y = 0;
 }
 
- function draw(e, canvasNum) {
+function draw(e, canvasNum) {
     var pos = getMousePos(canvasArray[canvasNum], e);
     posx = pos.x;
     posy = pos.y;
 
-    var drawString = "#draw"+canvasNum ;
+    var drawString = "#draw" + canvasNum;
     if ($(drawString).hasClass("active")) {
-     	var rect = new Box;
-		rect.x = posx;
-		rect.y = posy;
-		boxes[canvasNum].push(rect);
+        var rect = new Box;
+        rect.x = posx;
+        rect.y = posy;
+        boxes[canvasNum].push(rect);
 
-		contextArray[canvasNum].fillStyle="#FF0000";
-		contextArray[canvasNum].fillRect(rect.x,rect.y,width,height);
-    	count[canvasNum] = count[canvasNum] + 1 ;
-        var countedString = "#counted"+canvasNum ;
-    	$( countedString ).html( count[canvasNum] );
-    }
-    else {
-    	
-    	for (var i = 0; i < boxes[canvasNum].length; i++) {
-    		if( (posx > boxes[canvasNum][i].x) && (posx < boxes[canvasNum][i].x + width) && (posy > boxes[canvasNum][i].y) && (posy < boxes[canvasNum][i].y + height)) {
-    			contextArray[canvasNum].clearRect(boxes[canvasNum][i].x, boxes[canvasNum][i].y, width, height);
-    			boxes[canvasNum].splice(i, 1) ; // at position i remove 1 
+        contextArray[canvasNum].fillStyle = "#FF0000";
+        contextArray[canvasNum].fillRect(rect.x, rect.y, width, height);
+        count[canvasNum] = count[canvasNum] + 1;
+        var countedString = "#counted" + canvasNum;
+        $(countedString).html(count[canvasNum]);
+    } else {
 
-    			count[canvasNum] = count[canvasNum] - 1 ;
-                var countedString = "#counted"+canvasNum ;
-				$( countedString ).html( count[canvasNum] );
+        for (var i = 0; i < boxes[canvasNum].length; i++) {
+            if ((posx > boxes[canvasNum][i].x) && (posx < boxes[canvasNum][i].x + width) && (posy > boxes[canvasNum][i].y) && (posy < boxes[canvasNum][i].y + height)) {
+                contextArray[canvasNum].clearRect(boxes[canvasNum][i].x, boxes[canvasNum][i].y, width, height);
+                boxes[canvasNum].splice(i, 1); // at position i remove 1 
 
-    		}
-		}
+                count[canvasNum] = count[canvasNum] - 1;
+                var countedString = "#counted" + canvasNum;
+                $(countedString).html(count[canvasNum]);
+            }
+        }
     }
 }
 
 function eraseAll(canvasNum) {
-	for (var i = 0; i < boxes[canvasNum].length; i++) {
-    	contextArray[canvasNum].clearRect(boxes[canvasNum][i].x, boxes[canvasNum][i].y, width, height);
-	}
-	boxes[canvasNum] = []; 
+    for (var i = 0; i < boxes[canvasNum].length; i++) {
+        contextArray[canvasNum].clearRect(boxes[canvasNum][i].x, boxes[canvasNum][i].y, width, height);
+    }
+    boxes[canvasNum] = [];
 
-	count[canvasNum] = 0 ;
-    var countedString = "#counted"+canvasNum ;
-	$( countedString ).html( count[canvasNum] );
+    count[canvasNum] = 0;
+    var countedString = "#counted" + canvasNum;
+    $(countedString).html(count[canvasNum]);
 }
 
 function getMousePos(canvas, evt) {
     var rect = canvas.getBoundingClientRect();
     return {
-      x: evt.clientX - rect.left,
-      y: evt.clientY - rect.top
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
     };
 }
